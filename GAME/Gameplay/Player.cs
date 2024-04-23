a3ced04105d0070bf6f2c91e7690979daf45e0d0
Moises <moisesgabrielart@gmail.com>
2024-04-23 18:45:50 -0400

    player class

---
+++ GAME/Gameplay/Player.cs
@@ -0,0 +1,16 @@
+using Kai_Engine.ENGINE.Entities;
+
+namespace Kai_Engine.GAME.Gameplay
+{
+    public class Player 
+    {
+        public List<GameObject> SurroundingObjects = new();
+        public void DetectCross()
+        {
+            //Detect the surrounding objects in a cross pattern
+            //Once objects are found, add to SurroundingObjects list
+            //LOGIC GOES HERE
+            //Remove all objects from list
+        }
+    }
+}
